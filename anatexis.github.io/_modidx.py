43ffefc16bf3e3ae1aed241183e70df7c0381018
anatexis <anatexis@users.noreply.github.com>
2023-05-23 11:39:16 +0200

    docs only https://nbdev.fast.ai/tutorials/docs_only.html

--- anatexis.github.io/_modidx.py
+++ anatexis.github.io/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://anatexis.github.io',
                 'git_url': 'https://github.com/anatexis/anatexis.github.io',
                 'lib_path': 'anatexis.github.io'},
-  'syms': {'anatexis.github.io.core': {'anatexis.github.io.core.foo': ('core.html#foo', 'anatexis.github.io/core.py')}}}
+  'syms': {}}
